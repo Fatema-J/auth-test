a19f40e399d8fec0a089ba180d87cc4d642bd505
Fatem J <fatima.aljunaid.257@gmail.com>
2024-08-23 20:00:00 +0300

    changed the website

--- tests/authenticationTest.js
+++ tests/authenticationTest.js
@@ -3,28 +3,21 @@ import { Selector } from 'testcafe';
 // source: https://testcafe.io/documentation/402831/guides/basic-guides/test-structure
 // Fixtures are groups of tests that share the same starting URL.
 // better to have one per file
-fixture`Test structure`.page`https://automationexercise.com/login`
+fixture`Test structure`.page`https://thinking-tester-contact-list.herokuapp.com/login`
 
 // first arg is the name of the test
 test('SignUp The User, SignOut and SignIn Test', async (t) => {
   // Sign up process
   await t
-    .typeText('[data-qa="signup-name"]', 'Aisha')
-    .typeText('[data-qa="signup-email"]', 'aisha@gmail.com')
-    .click('[data-qa="signup-button"]')
+    .click('#signup')
+    .typeText('#firstName', 'Aisha')
+    .typeText('#lastName', 'Khalid') 
+    .typeText('#email', 'aisha@gmail.com')
     .typeText('#password', '12345678')
-    .typeText('#first_name', 'Aisha')
-    .typeText('#last_name', 'Khalid')
-    .typeText(
-      '#address1',
-      'Company Name: Tech Innovators Inc. Street Address: 1234 P.O. Box: P.O. Box 5678'
-  )
-    .click('#country')
-    .click(Selector('option').withText('United States'))
-    .typeText('#state', 'Capital')
-    .typeText('#city', 'Manama')
-    .typeText('#zipcode', '00000')
-    .typeText('#mobile_number', '38383838')
-    .click('[data-qa="create-account"]')
+    .click('#submit')
   
+  await t
+  .click('#logout')
+  
+
 })
