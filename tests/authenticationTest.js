73e5777beb06ba8601e5ef16a1f59c9950e413fc
Fatem J <fatima.aljunaid.257@gmail.com>
2024-08-23 20:19:22 +0300

    created full automation for the authentication

--- tests/authenticationTest.js
+++ tests/authenticationTest.js
@@ -1,23 +1,30 @@
-import { Selector } from 'testcafe';
+import { Selector } from 'testcafe'
 
 // source: https://testcafe.io/documentation/402831/guides/basic-guides/test-structure
 // Fixtures are groups of tests that share the same starting URL.
 // better to have one per file
-fixture`Test structure`.page`https://thinking-tester-contact-list.herokuapp.com/login`
+fixture`Test structure`
+  .page`https://thinking-tester-contact-list.herokuapp.com/`
 
 // first arg is the name of the test
 test('SignUp The User, SignOut and SignIn Test', async (t) => {
   // Sign up process
   await t
     .click('#signup')
-    .typeText('#firstName', 'Aisha')
-    .typeText('#lastName', 'Khalid') 
-    .typeText('#email', 'aisha@gmail.com')
+    .typeText('#firstName', 'Sara')
+    .typeText('#lastName', 'Khalid')
+    .typeText('#email', 'Sara12300@gmail.com')
     .typeText('#password', '12345678')
     .click('#submit')
-  
-  await t
-  .click('#logout')
-  
+    .expect(Selector('#add-contact').exists)
+    .ok()
 
+  // Sign out then Sign in
+  await t
+    .click('#logout')
+    .typeText('#email', 'Sara12300@gmail.com')
+    .typeText('#password', '12345678')
+    .click('#submit')
+    .expect(Selector('#add-contact').exists)
+    .ok()
 })
