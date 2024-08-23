cd6a4a95f13f3d1b7394e0461fefd27e87d6e1e9
Fatem J <fatima.aljunaid.257@gmail.com>
2024-08-23 23:31:53 +0300

    testing sign in with wrong credentials

--- authenticationTest.js
+++ authenticationTest.js
@@ -1,3 +1,4 @@
+import { Selector } from 'testcafe'
 import page from './authenticationPageModel'
 
 // source: https://testcafe.io/documentation/402831/guides/basic-guides/test-structure
@@ -23,4 +24,11 @@ test('SignUp The User, SignOut and SignIn Test', async (t) => {
   await t.click('#logout')
   await page.SignIn('Sara12300@gmail.com', '12345678')
   await t.expect(page.addContact.exists).ok()
+})
+
+test.only('Signing in with Unregistered User', async (t) => {
+  await page.SignIn('delmonia@gmail.com', '0987654321')
+  await t
+    .expect(Selector('#error').innerText)
+    .contains('Incorrect username or password')
 })
